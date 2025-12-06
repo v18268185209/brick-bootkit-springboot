f0703cf1b89851ec7922389fee92955736e04441
vim <huzhenjie@rjnetwork.net.cn>
2025-12-06 14:08:04 +0800

    调整目录结构

--- docs-website/vite.config.js
+++ docs-website/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: '../website',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  }
 })
